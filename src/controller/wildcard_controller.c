f298730a31c6ec70952ce4027f6596a3f282477e
Rohan Dvivedi <rohan.dvivedi@gmail.com>
2020-02-03 22:04:51 +0100

    wild card controller works, but only for wald card in method

---
+++ src/controller/wildcard_controller.c
@@ -0,0 +1,9 @@
+#include<http_request.h>
+#include<http_response.h>
+
+int wildcard_controller(HttpRequest* hrq, HttpResponse* hrp)
+{
+	printRequest(hrq);
+	append_to_dstring(hrp->body, "Hello, from the wildcard_controller");
+	return 0;
+}
