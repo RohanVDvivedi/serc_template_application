45f15ea1fda37199dc3d67e5fba854e56560137c
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-12 18:10:08 +0530

    modifications to serc_templtate to support serc

--- src/controller/first_controller.c
+++ src/controller/first_controller.c
@@ -4,6 +4,6 @@
 int first_controller(HttpRequest* hrq, HttpResponse* hrp)
 {
 	printRequest(hrq);
-	append_to_dstring(hrp->body, "Hello World");
+	append_to_dstring(&(hrp->body), "Hello World");
 	return 0;
 }
--- src/controller/wildcard_controller.c
+++ src/controller/wildcard_controller.c
@@ -4,6 +4,6 @@
 int wildcard_controller(HttpRequest* hrq, HttpResponse* hrp)
 {
 	printRequest(hrq);
-	append_to_dstring(hrp->body, "Hello, from the wildcard_controller");
+	append_to_dstring(&(hrp->body), "Hello, from the wildcard_controller");
 	return 0;
 }
