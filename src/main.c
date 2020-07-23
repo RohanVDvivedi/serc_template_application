316c304b80fa57326e95e1fc1a1871c96d0309c8
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-23 18:01:01 +0530

    conforming to the serc api of connection call bacl functions

--- src/main.c
+++ src/main.c
@@ -8,10 +8,10 @@
 int main()
 {
 	// HTTP server without SSL on port 80
-	//http_server_run(80, SERC_ROOT_PATH, 0);
+	//http_server_run(80, SERC_ROOT_PATH, 0 NULL, NULL);
 
 	// HTTP server with SSL on port 443
-	http_server_run(443, SERC_ROOT_PATH, 1);
+	http_server_run(443, SERC_ROOT_PATH, 1, NULL, NULL);
 
 	return 0;
 }
