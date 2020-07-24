21990faac219d28d557fd0feef23c095c88f9d0c
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-24 20:10:20 +0530

    conforming to the new serc api

--- src/main.c
+++ src/main.c
@@ -8,10 +8,10 @@
 int main()
 {
 	// HTTP server without SSL on port 80
-	//http_server_run(80, SERC_ROOT_PATH, 0 NULL, NULL, NULL);
+	//http_server_run(80, SERC_ROOT_PATH, 0);
 
 	// HTTP server with SSL on port 443
-	http_server_run(443, SERC_ROOT_PATH, 1, NULL, NULL, NULL);
+	http_server_run(443, SERC_ROOT_PATH, 1);
 
 	return 0;
 }
