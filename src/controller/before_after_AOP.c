d5a52c6a202c3956549bbd5fb274761b668f779b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-11 21:26:26 -0700

    fixed before and after controller

--- src/controller/before_after_AOP.c
+++ src/controller/before_after_AOP.c
@@ -2,15 +2,21 @@
 #include<http_response.h>
 
 #include<stdio.h>
+#include<stdlib.h>
 
-int call_before_controller(HttpRequest* hrq, HttpResponse* hrp)
+int call_before_controller(http_request_head* hrq, stream* strm, void** per_request_param, const void* server_param)
 {
-	printRequest(hrq);
+	*per_request_param = malloc(sizeof(int));
+	*((int*)(*per_request_param)) = rand();
+	printf("before : %d\n", *((int*)(*per_request_param)));
+	print_http_request_head(hrq);
 	return 0;
 }
 
-int call_after_controller(HttpRequest* hrq, HttpResponse* hrp)
+int call_after_controller(http_request_head* hrq, stream* strm, void** per_request_param, const void* server_param)
 {
-	printResponse(hrp);
+	printf("before : %d\n", *((int*)(*per_request_param)));
+	print_http_request_head(hrq);
+	free(*per_request_param);
 	return 0;
 }
