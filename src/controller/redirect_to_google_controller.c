f7e652342da1f865763074d6771bb164f18d3ff6
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-14 03:50:32 -0700

    need full url to redirect to some other website

--- src/controller/redirect_to_google_controller.c
+++ src/controller/redirect_to_google_controller.c
@@ -12,7 +12,7 @@ int redirect_to_google_controller(http_request_head* hrq, stream* strm, void* pe
 	// initialize response head
 	http_response_head hrp;
 	init_http_response_head_from_http_request_head(&hrp, hrq, 303, 0);
-	insert_in_dmap(&(hrp.headers), &get_dstring_pointing_to_literal_cstring("location"), &get_dstring_pointing_to_literal_cstring("google.com"));
+	insert_in_dmap(&(hrp.headers), &get_dstring_pointing_to_literal_cstring("location"), &get_dstring_pointing_to_literal_cstring("http://google.com"));
 
 	// write http response head
 	if(-1 == serialize_http_response_head(strm, &hrp))
