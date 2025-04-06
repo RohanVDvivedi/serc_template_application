8aa063b400a3ffa5d246ee4cdf98d9ef57bb1bc0
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-06 21:50:09 +0530

    refactored cookie setup contoller to use deferred callbacks from cutlery

--- src/controller/cookie_setup_controller.c
+++ src/controller/cookie_setup_controller.c
@@ -4,26 +4,31 @@
 
 #include<cutlery/stream_util.h>
 
+#include<cutlery/deferred_callbacks.h>
+
 #include<stdio.h>
 #include<stdint.h>
 
-// this global varibale is not protected, since it is only for an example for testing cooking get and set fnctionality
+// this global varibale is not protected, since it is only for an example for testing cooking get and set functionality
 int client_count = 0;
 
 int cookie_setup_controller(http_request_head* hrq, stream* strm, void* per_request_param, const void* server_param)
 {
+	NEW_DEFERRED_CALLS(16);
+
 	int close_connection = 0;
 
 	dmap cookies;
 	if(!init_dmap(&cookies, 0))
 	{
 		close_connection = 1;
-		goto EXIT_C_0;
+		goto EXIT;
 	}
+	DEFER(deinit_dmap, &cookies);
 	if(HTTP_NO_ERROR != parse_cookies_from_cookie_header(&cookies, &(hrq->headers)))
 	{
 		close_connection = 1;
-		goto EXIT_C_1;
+		goto EXIT;
 	}
 
 	// initialize response head
@@ -31,12 +36,13 @@ int cookie_setup_controller(http_request_head* hrq, stream* strm, void* per_requ
 	if(!init_http_response_head_from_http_request_head(&hrp, hrq, 200, TRANSFER_CHUNKED))
 	{
 		close_connection = 1;
-		goto EXIT_C_1;
+		goto EXIT;
 	}
+	DEFER(deinit_http_response_head, &hrp);
 	if(!insert_in_dmap(&(hrp.headers), &get_dstring_pointing_to_literal_cstring("content-type"), &get_dstring_pointing_to_literal_cstring("text/plain")))
 	{
 		close_connection = 1;
-		goto EXIT_C_2;
+		goto EXIT;
 	}
 
 	if(get_element_count_hashmap(&cookies) == 0)
@@ -45,7 +51,7 @@ int cookie_setup_controller(http_request_head* hrq, stream* strm, void* per_requ
 		if(!insert_formatted_in_dmap(&(hrp.headers), &get_dstring_pointing_to_literal_cstring("set-cookie"), "client_count=%d", client_count))
 		{
 			close_connection = 1;
-			goto EXIT_C_2;
+			goto EXIT;
 		}
 	}
 
@@ -53,21 +59,23 @@ int cookie_setup_controller(http_request_head* hrq, stream* strm, void* per_requ
 	if(HTTP_NO_ERROR != serialize_http_response_head(strm, &hrp))
 	{
 		close_connection = 1;
-		goto EXIT_C_2;
+		goto EXIT;
 	}
 
 	stacked_stream sstrm;
 	if(!initialize_stacked_stream(&sstrm))
 	{
 		close_connection = 1;
-		goto EXIT_C_2;
+		goto EXIT;
 	}
+	DEFER(deinitialize_stacked_stream, &sstrm);
 
 	if(0 > intialize_http_body_and_encoding_streams_for_writing(&sstrm, strm, &(hrp.headers)))
 	{
 		close_connection = 1;
-		goto EXIT_C_3;
+		goto EXIT;
 	}
+	DEFER(close_deinitialize_free_all_from_WRITER_stacked_stream, &sstrm);
 
 	int error = 0;
 
@@ -75,28 +83,17 @@ int cookie_setup_controller(http_request_head* hrq, stream* strm, void* per_requ
 	if(error)
 	{
 		close_connection = 1;
-		goto EXIT_C_4;
+		goto EXIT;
 	}
 
 	flush_all_from_stream(get_top_of_stacked_stream(&sstrm, WRITE_STREAMS), &error);
 	if(error)
 	{
 		close_connection = 1;
-		goto EXIT_C_4;
+		goto EXIT;
 	}
 
-	EXIT_C_4:;
-	close_deinitialize_free_all_from_stacked_stream(&sstrm, WRITE_STREAMS);
-
-	EXIT_C_3:;
-	deinitialize_stacked_stream(&sstrm);
-
-	EXIT_C_2:;
-	deinit_http_response_head(&hrp);
-
-	EXIT_C_1:;
-	deinit_dmap(&cookies);
-
-	EXIT_C_0:;
+	EXIT:;
+	CALL_ALL_DEFERRED();
 	return close_connection;
 }
